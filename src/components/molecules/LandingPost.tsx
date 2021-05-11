6f4daeeda1fbd8ff1dac5456ec9dac557076a9e6
negiseijin <satoru6221@gmail.com>
2021-05-11 13:24:11 +0000

    :up: UIを少し改善

--- src/components/molecules/LandingPost.tsx
+++ src/components/molecules/LandingPost.tsx
@@ -12,36 +12,54 @@ type Props = {
 
 export const LandingPost: React.VFC<Props> = React.memo(({ post }) => {
   return (
-    <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={post.title} src={post.coverImage} id={post.id} />
-      </div>
-      <div className="md:grid md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-          {post.title}
-        </h3>
-        <div className="my-4">
-          {post.category.map((category) => (
-            <Badge
-              key={category.id}
-              bgColor="bg-indigo-700"
-              color="text-indigo-100"
-              name={category.name}
-            />
-          ))}
+    <div className="md:flex bg-gray-100 rounded-xl p-8 md:p-0 shadow-lg">
+      <section className="bg-white rounded-lg">
+        <div className="mb-4 md:mb-8">
+          <CoverImage title={post.title} src={post.coverImage} id={post.id} />
         </div>
-        <div className="mb-4 md:mb-0 text-lg">
-          <DateFormatter dateString={post.date} />
+        <div className="grid gap-4 pb-8 mx-4">
+          <h3 className="mx-4 text-4xl lg:text-6xl leading-tight">
+            {post.title}
+          </h3>
+          <div className="mx-4">
+            {post.category.map((category) => (
+              <Badge
+                key={category.id}
+                bgColor="bg-indigo-700"
+                color="text-indigo-100"
+                name={category.name}
+              />
+            ))}
+          </div>
+          <div className="flex flex-row mx-4 text-lg">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <DateFormatter dateString={post.date} />
+          </div>
+          <div className="mx-4">
+            <p className="text-lg leading-relaxed">{post.description}</p>
+          </div>
+          <div className="mx-4">
+            <Link as={`/blog/${post.id}`} href="/blog/[id]">
+              <a className="text-lg font-medium text-red-500 underline hover:no-underline">
+                Read more
+              </a>
+            </Link>
+          </div>
         </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{post.description}</p>
-          <Link as={`/blog/${post.id}`} href="/blog/[id]">
-            <a className="text-lg font-medium text-red-500 underline hover:no-underline">
-              Read more
-            </a>
-          </Link>
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 })
--- src/components/molecules/MoreStories.tsx
+++ src/components/molecules/MoreStories.tsx
@@ -10,10 +10,11 @@ type Props = {
 export const MoreStories: React.VFC<Props> = React.memo(({ posts }) => {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+      <h2 className="my-4 md:my-12 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         More Stories
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+
+      <div className="p-8 grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32 bg-gray-100 rounded-xl">
         {posts.map((post) => (
           <PostPreview key={post.id} post={post} />
         ))}
